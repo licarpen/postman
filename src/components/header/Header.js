a9a31763c3ef6b7125a0f404d1acfb5a9fd5c781
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-03-22 14:36:49 -0700

    update snapshots

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.css';
 const Header = () => {
   return (
     <div className={styles.Header}>
-      <h1>Postman</h1>
+      <h1>POSTMAN</h1>
       <h2>A serverside application for visualizing responses to RESTful requests</h2>
     </div>
   );
